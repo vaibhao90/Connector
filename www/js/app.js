ea70672e02fe9fd29f20d72742e37359cb41becf
vaibhao90 <vaibhao.dumore75@gmail.com>
2016-11-08 01:37:28 +0530

    updated app

--- www/js/app.js
+++ www/js/app.js
@@ -4,94 +4,135 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ionic-datepicker'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ionic-datepicker', 'ngStorage'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-.config(function (ionicDatePickerProvider) {
-    var datePickerObj = {
-      inputDate: new Date(),
-      titleLabel: 'Select a Date',
-      setLabel: 'Set',
-      todayLabel: 'Today',
-      closeLabel: 'Close',
-      mondayFirst: false,
-      weeksList: ["S", "M", "T", "W", "T", "F", "S"],
-      monthsList: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
-      templateType: 'popup',
-      from: new Date(2012, 8, 1),
-      to: new Date(2018, 8, 1),
-      showTodayButton: true,
-      dateFormat: 'dd MMMM yyyy',
-      closeOnSelect: false,
-      disableWeekdays: []
-    };
-    ionicDatePickerProvider.configDatePicker(datePickerObj);
-  })
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    .config(function(ionicDatePickerProvider) {
+        var datePickerObj = {
+            inputDate: new Date(),
+            titleLabel: 'Select a Date',
+            setLabel: 'Set',
+            todayLabel: 'Today',
+            closeLabel: 'Close',
+            mondayFirst: false,
+            weeksList: ["S", "M", "T", "W", "T", "F", "S"],
+            monthsList: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+            templateType: 'popup',
+            from: new Date(2012, 8, 1),
+            to: new Date(2018, 8, 1),
+            showTodayButton: true,
+            dateFormat: 'dd MMMM yyyy',
+            closeOnSelect: false,
+            disableWeekdays: []
+        };
+        ionicDatePickerProvider.configDatePicker(datePickerObj);
+    })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+        $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+            .state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'templates/menu.html',
+            controller: 'AppCtrl'
+        })
 
-  .state('app.home', {
-    url: '/home',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/home.html',
-        controller: 'HomePageCtrl'
-      }
-    }
-  })
+        .state('app.login', {
+            url: '/login',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/login_scene.html',
+                    controller: 'LoginPageCtrl'
+                }
+            }
+        })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
+        .state('app.home', {
+            url: '/home',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/home.html',
+                    controller: 'HomePageCtrl'
+                }
+            }
+        })
+
+        .state('app.contacts', {
+                url: '/contacts',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/browse.html'
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: '/playlists',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlists.html',
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
+
+        .state('app.single', {
+            url: '/playlists/:playlistId',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/playlist.html',
+                    controller: 'PlaylistCtrl'
+                }
+            }
+        })
+
+        .state('app.sky', {
+            url: '/sky',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/playlist.html',
+                    controller: 'PlaylistCtrl'
+                }
+            }
+        });
+        // if none of the above states are matched, use this as the fallback
+
+        $urlRouterProvider.otherwise('/app/home');
     })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+    .factory('StorageService', function($localStorage) {
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+        var _getAll = function() {
+            return $localStorage.things;
+        };
+
+        var _add = function(thing) {
+            $localStorage.things.push(thing);
+        }
+
+        var _remove = function(thing) {
+            $localStorage.things.splice($localStorage.things.indexOf(thing), 1);
+        }
+
+        return {
+            getAll: _getAll,
+            add: _add,
+            remove: _remove
+        };
+    })
 
 // .directive('daterangepicker', ['$parse', '$rootScope', function($parse, $rootScope) {
 //     return {
@@ -135,6 +176,3 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic-datepicker'])
 //        }      
 //     }
 // }]);
-
-
-
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,113 +1,160 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $rootScope, StorageService, $state) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+  
+   
 
-  // Form data for the login modal
-  $scope.loginData = {};
+   
+    $rootScope.openModal = function() {
+         // Form data for the login modal
+        $rootScope.authenticatedUser = false;
+        $rootScope.checkLogin();
+    };
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+    // Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+        // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+        // Execute action
+    });
+    $rootScope.authenticatedUser = {};
+    $rootScope.loginData = { username:  "tester@yopmail.com", password: "Password@vd01"};
+    $rootScope.authenticated = false;
+   
+ $rootScope.doLogin = function(loginData) {
+      console.log(" loginData", loginData);
+       if(loginData.username && loginData.password && loginData.username == "tester@yopmail.com" && loginData.password == "Password@vd01"){
+             $timeout(function() {
+                $rootScope.authenticated = true;
+                $rootScope.authenticatedUser = loginData;
+                $rootScope.authenticatedUser.fullname= "John Doe";
+                $rootScope.authenticatedUser.profile_image = "img/prof2.jpg";
+                $rootScope.modal.hide();
+                $state.go("app.home");
+            }, 1000);
+       }else{
+        $rootScope.loginMessage = "Please enter valid "
+       }
+      
+    };
+    
+     $rootScope.checkLogin = function(){
+      var items = StorageService.getAll() ? StorageService.getAll() : {};
+      $rootScope.authenticatedUser = items.authenticatedUser || {};
+      $rootScope.authenticated =  items.authenticated || false;
+      if(!$rootScope.authenticated) {
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $rootScope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $rootScope.modal = modal;  
+                $rootScope.modal.show();
+  
+    });
+      }
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    } 
+})
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+.controller('LoginPageCtrl', function($scope, $rootScope, $timeout, $state) {
+    $rootScope.authenticated = false;
+    $rootScope.currentUser = {};
+    console.log(" State ", $state);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+        console.log('Doing login', $scope.loginData);
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+            $rootScope.authenticated = true;
+            $state.go("app.home");
+        }, 1000);
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
 .controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+    $scope.playlists = [
+        { title: 'Reggae', id: 1 },
+        { title: 'Chill', id: 2 },
+        { title: 'Dubstep', id: 3 },
+        { title: 'Indie', id: 4 },
+        { title: 'Rap', id: 5 },
+        { title: 'Cowbell', id: 6 }
+    ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-})
-.controller('HomePageCtrl', function($scope, $stateParams, ionicDatePicker) {
-  $scope.startDate =  new Date();
-  $scope.endDate =  new Date();
-   var ipObj1 = {
-      callback: function (val) {  //Mandatory
-        // console.log('Return value from the datepicker popup is : ' + val, new Date(val));
-         $scope.startDate =  new Date(val);
-      },
-      disabledDates: [            //Optional
-        // new Date(2016, 2, 16),
-        // new Date(2015, 3, 16),
-        // new Date(2015, 4, 16),
-        // new Date(2015, 5, 16),
-        // new Date('Wednesday, August 12, 2015'),
-        // new Date("08-16-2016"),
-        // new Date(1439676000000)
-      ],
-      from: new Date(2012, 1, 1), //Optional
-      to: new Date(2016, 10, 30), //Optional
-      inputDate: new Date(),      //Optional
-      mondayFirst: true,          //Optional
-      disableWeekdays: [0],       //Optional
-      closeOnSelect: false,       //Optional
-      templateType: 'popup'       //Optional
-    };
-    var ipObj2 = {
-      callback: function (val) {  //Mandatory
-        // console.log('Return value from the datepicker popup is : ' + val, new Date(val));
-        $scope.endDate =  new Date(val);
-        // return  startDate
-      },
-      disabledDates: [            //Optional
-        // new Date(2016, 2, 16),
-        // new Date(2015, 3, 16),
-        // new Date(2015, 4, 16),
-        // new Date(2015, 5, 16),
-        // new Date('Wednesday, August 12, 2015'),
-        // new Date("08-16-2016"),
-        // new Date(1439676000000)
-      ],
-      from: new Date(2012, 1, 1), //Optional
-      to: new Date(2016, 10, 30), //Optional
-      inputDate: new Date(),      //Optional
-      mondayFirst: true,          //Optional
-      disableWeekdays: [0],       //Optional
-      closeOnSelect: false,       //Optional
-      templateType: 'popup'       //Optional
-    };
+.controller('PlaylistCtrl', function($scope, $stateParams) {})
+    .controller('HomePageCtrl', function($scope, $stateParams, ionicDatePicker) {
+        $scope.startDate = new Date();
+        $scope.endDate = new Date();
+        var ipObj1 = {
+            callback: function(val) { //Mandatory
+                // console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+                $scope.startDate = new Date(val);
+            },
+            disabledDates: [ //Optional
+                // new Date(2016, 2, 16),
+                // new Date(2015, 3, 16),
+                // new Date(2015, 4, 16),
+                // new Date(2015, 5, 16),
+                // new Date('Wednesday, August 12, 2015'),
+                // new Date("08-16-2016"),
+                // new Date(1439676000000)
+            ],
+            from: new Date(2012, 1, 1), //Optional
+            to: new Date(2016, 10, 30), //Optional
+            inputDate: new Date(), //Optional
+            mondayFirst: true, //Optional
+            disableWeekdays: [0], //Optional
+            closeOnSelect: false, //Optional
+            templateType: 'popup' //Optional
+        };
+        var ipObj2 = {
+            callback: function(val) { //Mandatory
+                // console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+                $scope.endDate = new Date(val);
+                // return  startDate
+            },
+            disabledDates: [ //Optional
+                // new Date(2016, 2, 16),
+                // new Date(2015, 3, 16),
+                // new Date(2015, 4, 16),
+                // new Date(2015, 5, 16),
+                // new Date('Wednesday, August 12, 2015'),
+                // new Date("08-16-2016"),
+                // new Date(1439676000000)
+            ],
+            from: new Date(2012, 1, 1), //Optional
+            to: new Date(2016, 10, 30), //Optional
+            inputDate: new Date(), //Optional
+            mondayFirst: true, //Optional
+            disableWeekdays: [0], //Optional
+            closeOnSelect: false, //Optional
+            templateType: 'popup' //Optional
+        };
 
-    $scope.openDatePicker = function(){
-      ionicDatePicker.openDatePicker(ipObj1);
-    };
+        $scope.openDatePicker = function() {
+            ionicDatePicker.openDatePicker(ipObj1);
+        };
 
-    $scope.openDatePicker2 = function(){
-      ionicDatePicker.openDatePicker(ipObj2);
-    };
-});
+        $scope.openDatePicker2 = function() {
+            ionicDatePicker.openDatePicker(ipObj2);
+        };
+    });
